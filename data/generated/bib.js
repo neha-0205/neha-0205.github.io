45ed60ecdfd761932ecf1811a884b531abcd9894
neha-0205 <126343753+neha-0205@users.noreply.github.com>
2023-05-15 15:50:34 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,112 @@
+﻿define({ entries : {
+    "10020685": {
+        "author": "Yeo, Hangu and Khorasani, Elahe and Sheinin, Vadim and Manotas, Irene and An Vo, Ngoc Phuoc and Popescu, Octavian and Zerfos, Petros",
+        "booktitle": "2022 IEEE International Conference on Big Data (Big Data)",
+        "doi": "10.1109/BigData55660.2022.10020685",
+        "number": "",
+        "pages": "4443-4452",
+        "title": "Natural Language Interface for Process Mining Queries in Healthcare",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2022"
+    },
+    "5489508": {
+        "author": "Ibrahim, Mohd and Ahmad, Rodina",
+        "booktitle": "2010 Second International Conference on Computer Research and Development",
+        "doi": "10.1109/ICCRD.2010.71",
+        "number": "",
+        "pages": "200-204",
+        "title": "Class Diagram Extraction from Textual Requirements Using Natural Language Processing (NLP) Techniques",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2010"
+    },
+    "7203085": {
+        "author": "Weigelt, Sebastian and Tichy, Walter F.",
+        "booktitle": "2015 IEEE/ACM 37th IEEE International Conference on Software Engineering",
+        "doi": "10.1109/ICSE.2015.264",
+        "number": "",
+        "pages": "819-820",
+        "title": "Poster: ProNat: An Agent-Based System Design for Programming in Spoken Natural Language",
+        "type": "INPROCEEDINGS",
+        "volume": "2",
+        "year": "2015"
+    },
+    "7275834": {
+        "author": "Johanna Johnsi Rani G and Gladis, Dennis and Manipadam, Marie Therese and Ishitha, Gunadala",
+        "booktitle": "2015 International Conference on Advances in Computing, Communications and Informatics (ICACCI)",
+        "doi": "10.1109/ICACCI.2015.7275834",
+        "number": "",
+        "pages": "1552-1558",
+        "title": "Breast cancer staging using Natural Language Processing",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2015"
+    },
+    "7591245": {
+        "author": "Vilic, Adnan and Petersen, John Asger and Hoppe, Karsten and Sorensen, Helge B. D.",
+        "booktitle": "2016 38th Annual International Conference of the IEEE Engineering in Medicine and Biology Society (EMBC)",
+        "doi": "10.1109/EMBC.2016.7591245",
+        "number": "",
+        "pages": "2529-2532",
+        "title": "Visualizing patient journals by combining vital signs monitoring and natural language processing",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2016"
+    },
+    "7845054": {
+        "author": "e Sameen Tabasswn, Dur and Siddiqui, Habiba and Ramzan, Shabana",
+        "booktitle": "2016 Sixth International Conference on Innovative Computing Technology (INTECH)",
+        "doi": "10.1109/INTECH.2016.7845054",
+        "number": "",
+        "pages": "621-626",
+        "title": "Natural language based constraints for web services",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2016"
+    },
+    "8212846": {
+        "author": "Gupta, Prashant and Goswami, Aman and Koul, Sahil and Sartape, Kashinath",
+        "booktitle": "2017 International conference of Electronics, Communication and Aerospace Technology (ICECA)",
+        "doi": "10.1109/ICECA.2017.8212846",
+        "number": "",
+        "pages": "410-413",
+        "title": "IQS-intelligent querying system using natural language processing",
+        "type": "INPROCEEDINGS",
+        "volume": "2",
+        "year": "2017"
+    },
+    "8644947": {
+        "author": "Tapsai, Chalermpol",
+        "booktitle": "2018 IEEE 3rd International Conference on Communication and Information Systems (ICCIS)",
+        "doi": "10.1109/ICOMIS.2018.8644947",
+        "number": "",
+        "pages": "212-216",
+        "title": "Information Processing and Retrieval from CSV File by Natural Language",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2018"
+    },
+    "9630371": {
+        "author": "Soni, Aradhana and Amrhein, Benjamin and Baucum, Matthew and Paek, Eun Jin and Khojandi, Anahita",
+        "booktitle": "2021 43rd Annual International Conference of the IEEE Engineering in Medicine & Biology Society (EMBC)",
+        "doi": "10.1109/EMBC46164.2021.9630371",
+        "number": "",
+        "pages": "2282-2285",
+        "title": "Using Verb Fluency, Natural Language Processing, and Machine Learning to Detect Alzheimer\u2019s Disease",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2021"
+    },
+    "9691740": {
+        "author": "Gogoi, Bronjon and Ahmed, Tasiruddin and Dutta, Arabinda",
+        "booktitle": "2021 IEEE 18th India Council International Conference (INDICON)",
+        "doi": "10.1109/INDICON52576.2021.9691740",
+        "number": "",
+        "pages": "1-6",
+        "title": "Defending against SQL Injection Attacks in Web Applications using Machine Learning and Natural Language Processing",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2021"
+    }
+}});
